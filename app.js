144714f369f3d5c41399fa855c772926051fb45d
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-07-19 22:47:30 +0200

    fix import

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // ------------------Routes------------------ //
-import { e, s, tryCatch } from "./utils";
+import { e, s, tryCatch } from "./utils.js";
 import fetch from "node-fetch";
 
 app.use("/", async (req, res) => { // url?http://example.com/
